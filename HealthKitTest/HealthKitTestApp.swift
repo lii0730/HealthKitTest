e168a377ad072849a6094a14220f22e4ba70ee11
LeeHsss <lii0730@icloud.com>
2021-07-19 16:56:50 +0900

    Initial Commit

---
+++ HealthKitTest/HealthKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitTestApp.swift
+//  HealthKitTest
+//
+//  Created by LeeHsss on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
